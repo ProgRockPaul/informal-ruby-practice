9b28ef09d21f38276bd642378617bff29e19e36a
Paul Walsh <twentytwentyk@live.com>
2017-01-16 18:08:17 -0500

    add ruby practice files

---
+++ rubypractice.rb
@@ -0,0 +1,30 @@
+
+# puts "Hey what\'s your first name?"
+# first = gets.chomp
+# puts "And, what\'s your middle name?"
+# middle = gets.chomp
+# puts "And, what\'s your last name?"
+# last = gets.chomp
+# puts "Your name is #{first} #{middle} #{last}"
+
+# puts "What\'s your favourite number"
+# num1 = gets.chomp.to_i
+# puts "Give me another and I'll add it."
+# num2 = gets.chomp.to_i
+# puts "Great! #{num1} + #{num2} = #{num1 + num2}"
+
+# puts "What\'s your favourite number"
+# num1 = gets.chomp.to_i
+# puts "That is great, but I think #{num1 * 3} is a wayyyy better number... "
+
+puts "Give me a fruit"
+fruit = gets.chomp.upcase
+puts "Give me an animal"
+animal = gets.chomp.upcase
+puts "Finally, give me a weapon"
+weapon = gets.chomp.upcase
+puts "#{animal} likes to eat #{fruit} while weilding a #{weapon}"
+
+if animal = "bear"
+  puts "I don't understand why bears like #{fruit}S and #{weapon}S !?!?"
+end
